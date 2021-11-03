8b98db76abac884be9241f44c2fc2de8dd00b57f
Will Brennan <willbrennan314@gmail.com>
2021-11-02 21:06:30 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(driver1, driver2) {
+ return ['Sally', 'Bob'];
+}
+
+const returnLastTwoDrivers = function(driver1, driver2) {
+    return ['Freddy', 'Claudia'];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+createFareMultiplier = (num) => fareMultiplier => num * 5;
+
+function fareDoubler(fareMultiplier) {
+    return fareMultiplier * 2;
+}
+
+function fareTripler(fareMultiplier) {
+    return fareMultiplier * 3;
+}
+
+function selectDifferentDrivers(arrayofDrivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(arrayofDrivers);
+}
